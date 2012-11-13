100a890c0d6abb3d430324e6b8c1b441706be548
blogic <blogic@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2012-11-13 13:14:57 +0000

    [lantiq] fixes dgn3500
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/attitude_adjustment@34184 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/lantiq/ar9/target.mk
+++ target/linux/lantiq/ar9/target.mk
@@ -5,7 +5,7 @@ FEATURES:=squashfs jffs2 atm
 
 DEFAULT_PACKAGES+=kmod-pppoa ppp-mod-pppoa linux-atm atm-tools br2684ctl kmod-ltq-dsl-ar9 ltq-dsl-app \
 	kmod-input-gpio-keys-polled kmod-ledtrig-netdev kmod-leds-gpio kmod-button-hotplug \
-	swconfig
+	swconfig wpad-mini
 
 define Target/Description
 	Lantiq AR9
