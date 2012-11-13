d07c50221a569a01ed083d0e04573ba3c3ef3e88
blogic <blogic@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2012-11-13 13:14:53 +0000

    [lantiq] vr9 i not really usable, lets just ignore it
    git-svn-id: svn://svn.openwrt.org/openwrt/branches/attitude_adjustment@34182 3c298f89-4303-0410-b956-a3cf2f4a3e73

--- target/linux/lantiq/vr9/target.mk
+++ target/linux/lantiq/vr9/target.mk
@@ -1,7 +1,7 @@
 ARCH:=mips
 SUBTARGET:=vr9
 BOARDNAME:=VR9
-FEATURES:=squashfs jffs2 atm
+FEATURES:=squashfs jffs2 atm broken
 
 DEFAULT_PACKAGES+=kmod-pppoa ppp-mod-pppoa linux-atm atm-tools br2684ctl kmod-ltq-dsl-vr9 ltq-dsl-app swconfig
 
